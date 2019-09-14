ba5721f8405c12caa9d835d9784decce4d2d9a32
Lucas Brendel <lucasbrendel@gmail.com>
2019-09-14 13:11:08 -0500

    correcting rustup component add

--- ci/install.sh
+++ ci/install.sh
@@ -4,8 +4,8 @@ set -e
 main() {
     if [ $TARGET != x86_64-unknown-linux-gnu ]; then
         rustup target add $TARGET
-        rustup component add rustfmt
-        rustup component add clippy
+        rustup component add rustfmt --toolchain nightly-x86_64-unknown-linux-gnu
+        rustup component add clippy --toolchain nightly-x86_64-unknown-linux-gnu
         cargo install cargo-update || echo "cargo-update already installed"
         cargo install cargo-travis || echo "cargo-travis already installed"
         cargo install-update -a
